23b0ec640721320c0d9bbd1f704ea601b5bd89b5
root <root@vmi1615983.contaboserver.net>
2024-02-14 02:46:32 +0100

    remove alpha files

--- storage/alpha-NLM/blueprint/extensions/blueprint/private/build/extensions/route.php
+++
@@ -1,1 +0,0 @@
-Route::group(['prefix' => 'extensions/␀id␀'], function () {Route::get('/', [Admin\Extensions\␀id␀\␀id␀ExtensionController::class, 'index'])->name('admin.extensions.␀id␀.index');Route::patch('/', [Admin\Extensions\␀id␀\␀id␀ExtensionController::class, 'update']);Route::post('/', [Admin\Extensions\␀id␀\␀id␀ExtensionController::class, 'post']);});
